c9f52b492ba94673a5a5488e892416ef87484a2b
Shreeyam <shreeyam@mit.edu>
2020-03-04 19:43:28 +0000

    Added varying pinf plot

---
+++ varying_pinf.m
@@ -0,0 +1,41 @@
+% Plot of nth and nprop for varying free stream pressure
+% AERO96016 Adv. Prop. Coursework
+% Created by Shreeyam Kacker 2020-03-04
+
+%% Housekeeping
+clc;
+clear;
+close all;
+
+%% Setup
+
+% Load default values
+defaults;
+Pinf = linspace(0, 100e5);
+
+nth = zeros(1, length(Pinf));
+nprop = zeros(1, length(Pinf));
+
+%% Run
+
+for i = 1:length(Pinf)
+    [~,~,~,~,~,~,nth(i),nprop(i)] = ramjet(Tinf, Pinf(i), Minf, Mx, M2, Tb, F);
+end
+
+%% Plots
+
+f = figure;
+hold on;
+
+plot(Pinf/1e3, nth);
+plot(Pinf/1e3, nprop);
+
+ylim([0, 1]);
+
+xlabel('P_{\infty} [kPa]')
+ylabel('\eta [-]');
+
+grid;
+improvePlot;
+
+set(f, 'Position', [10 10 900 600]);
